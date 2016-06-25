de9bdf1b41867d8302e9e9e010c65afa895cd71b
Roger Allen <rallen@gmail.com>
2016-06-25 08:44:46 -0700

    add gamma. draws pink quad

--- project.clj
+++ project.clj
@@ -5,12 +5,13 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :min-lein-version "2.6.1"
-  
+
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.8.51"]
                  [org.clojure/core.async "0.2.374"
-                  :exclusions [org.clojure/tools.reader]]]
-  
+                  :exclusions [org.clojure/tools.reader]]
+                 [kovasb/gamma "0.0-135"]]
+
   :plugins [[lein-figwheel "0.5.4-3"]
             [lein-cljsbuild "1.1.3" :exclusions [[org.clojure/clojure]]]]
 
@@ -60,7 +61,7 @@
              ;; if you want to embed a ring handler into the figwheel http-kit
              ;; server, this is for simple ring servers, if this
              ;; doesn't work for you just run your own server :) (see lien-ring)
-             
+
              ;; :ring-handler hello_world.server/handler
 
              ;; To be able to open files in your editor from the heads up display
@@ -71,7 +72,7 @@
              ;; emacsclient -n +$2 $1
              ;;
              ;; :open-file-command "myfile-opener"
-             
+
              ;; if you are using emacsclient you can just use
              ;; :open-file-command "emacsclient"
 
@@ -82,12 +83,12 @@
              ;; :server-logfile "tmp/logs/figwheel-logfile.log"
              }
 
- 
+
   ;; setting up nREPL for Figwheel and ClojureScript dev
   ;; Please see:
   ;; https://github.com/bhauman/lein-figwheel/wiki/Using-the-Figwheel-REPL-within-NRepl
-  
-  
+
+
   :profiles {:dev {:dependencies [[figwheel-sidecar "0.5.4-3"]
                                   [com.cemerick/piggieback "0.2.1"]]
                    ;; need to add dev source path here to get user.clj loaded
--- src/geemee/core.cljs
+++ src/geemee/core.cljs
@@ -1,14 +1,50 @@
 (ns geemee.core
-  (:require ))
+  (:require [gamma.api :as g]
+            [gamma.program :as p]
+            [goog.dom :as gdom]
+            [goog.webgl :as wgl]))
 
 (enable-console-print!)
 
-(println "This text is printed from src/geemee/core.cljs. Go ahead and edit it and see reloading in action.")
-
 ;; define your app data so that it doesn't get over-written on reload
-
 (defonce app-state (atom {:text "Hello world!"}))
 
+;; this state doesn't care about being overwritten on reload
+(def vertex-position (g/attribute "a_VertexPosition" :vec2))
+(def vertex-shader   {(g/gl-position) (g/vec4 vertex-position 0 1)})
+(def fragment-shader {(g/gl-frag-color) (g/vec4 1 0.25 0.5 1)})
+
+(defn main []
+  (let [prog (p/program {:vertex-shader vertex-shader
+                         :fragment-shader fragment-shader})
+        gl  (.getContext (gdom/getElement "gl-canvas") "webgl")
+        vs  (.createShader gl wgl/VERTEX_SHADER)
+        fs  (.createShader gl wgl/FRAGMENT_SHADER)
+        pgm (.createProgram gl)
+        xs  (js/Float32Array. #js [-1 -1 ;; bottom tri
+                                    1 -1
+                                   -1  1
+                                   -1  1 ;; top tri
+                                    1 -1
+                                    1  1])
+        buf (.createBuffer gl)]
+    (doto gl
+      (.shaderSource vs (-> prog :vertex-shader :glsl))
+      (.compileShader vs)
+      (.shaderSource fs (-> prog :fragment-shader :glsl))
+      (.compileShader fs)
+      (.attachShader pgm vs)
+      (.attachShader pgm fs)
+      (.linkProgram pgm)
+      (.bindBuffer wgl/ARRAY_BUFFER buf)
+      (.bufferData wgl/ARRAY_BUFFER xs wgl/STATIC_DRAW)
+      (.enableVertexAttribArray (.getAttribLocation gl pgm (:name vertex-position)))
+      (.vertexAttribPointer (.getAttribLocation gl pgm (:name vertex-position))
+                            2 wgl/FLOAT false 0 0)
+      (.useProgram pgm)
+      (.drawArrays wgl/TRIANGLES 0 6))))
+
+(main)
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
