b2b757dbb6255efa5af67e946b02f586a7b9effb
Roger Allen <rallen@gmail.com>
2016-07-18 06:40:43 -0700

    add geemee.api for future functions

---
+++ src/geemee/api.cljs
@@ -0,0 +1,14 @@
+(ns geemee.api)
+
+;; term-fns
+;;    noise, turbulance, etc.
+;; unary-fns
+;;    `square `vsqrt `sigmoid `max-component `min-component
+;;    `length `normalize `gradient
+;;    `hue-from-rgb `lightness-from-rgb `saturation-from-rgb
+;;    `hsl-from-rgb `red-from-hsl `green-from-hsl `blue-from-hsl
+;;    `rgb-from-hsl `x `y `z `t `alpha
+;; binary-fns
+;;    `vpow `vmod `dot `cross3
+;;    `vmin `vmax `checker `scale `offset
+;;    `adjust-hue `adjust-hsl `vconcat})
