070dfa1c26a20f681d8d69276c85483ef9f067e8
Roger Allen <rallen@gmail.com>
2016-08-09 09:27:18 -0700

    give proper credit to Chris Ford's Klangmeister

--- src/geemee/eval.cljs
+++ src/geemee/eval.cljs
@@ -2,8 +2,30 @@
   (:require [cljs.js :as cljs])
   (:require-macros [geemee.macros :as macro]))
 
-;; another wholesale ripoff of klangmeister
+;; This file's code was initially brought over from klangmeister.
+;; So, I'm including the required license.
+;;
 ;; https://github.com/ctford/klangmeister/blob/master/src/klangmeister/compile/eval.cljs
+;;
+;; Copyright (c) 2015, Chris Ford (christophertford at gmail)
+;;
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be included in
+;; all copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+;; THE SOFTWARE.
 
 (def namespace-declaration
   (macro/literally
@@ -36,18 +58,6 @@
 (defn normalise [result]
   (update result :error #(some-> % .-cause .-stack))) ;; .-message
 
-;; FIXME? this isn't working yet.
-(defn uate
-  "Evaluate a Clojurescript form."
-  [state expr]
-  (cljs/eval
-    state
-    (list '(ns geemee.live (:require [gamma.api :as g]))
-          expr)
-    {:eval cljs/js-eval
-     :load loader}
-    normalise))
-
 (defn uate-str
   "Evaluate a string of Clojurescript."
   [state expr-str]
@@ -55,6 +65,18 @@
     state
     (str namespace-declaration expr-str)
     nil
+    {:eval cljs/js-eval
+     :load loader}
+    normalise))
+
+;; this isn't working yet...use uate-str instead
+(defn uate
+  "Evaluate a Clojurescript form."
+  [state expr]
+  (cljs/eval
+    state
+    (list '(ns geemee.live (:require [gamma.api :as g]))
+          expr)
     {:eval cljs/js-eval
      :load loader}
     normalise))
--- src/geemee/macros.clj
+++ src/geemee/macros.clj
@@ -5,9 +5,31 @@
             [clojure.java.classpath :as classpath])
   (:import (java.io File)))
 
-;; grabbed wholesale from klangmeister
+;; This file's code was initially brought over from klangmeister.
+;; So, I'm including the required license.
+;; The code has been updated to handle gamma's macros & the clj-type of file loading.
+;;
 ;; https://github.com/ctford/klangmeister/blob/master/src/klangmeister/compile/macros.clj
-;; updated to handle gamma macros
+;;
+;; Copyright (c) 2015, Chris Ford (christophertford at gmail)
+;;
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be included in
+;; all copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+;; THE SOFTWARE.
 
 (def find_js {:extensions (list ".js")})
 
@@ -20,17 +42,12 @@
   (->> (classpath/classpath-directories)
        (mapcat #(find/find-sources-in-dir % of-type))))
 
-;;(spit "foo_files_js.txt" (pr-str (files find_js)))
-
 (defn jars
   "find files of-type [find/cljs find/clj or find_js] in your jars"
   [of-type]
   (->> (classpath/classpath-jarfiles)
        (mapcat #(find/sources-in-jar % of-type))
        (map io/resource)))
-
-;;(spit "foo_jars_clj.txt" (pr-str (jars find/clj)))
-;;(spit "foo_jars_js.txt" (pr-str (jars find_js)))
 
 (defn collate [entries]
   (reduce conj {} entries))
@@ -43,19 +60,20 @@
          (filter relevant?)
          collate)))
 
-;;(spit "foo_sources.txt" (sources* (list 'gamma.api)))
-
 (defmacro sources-cljs
-  "Make a map of namespace name to source, looking for files on the classpath and in jars."
+  "Make a map of CLJS namespace name to source, looking for files on
+  the classpath and in jars."
   [& names]
   (sources* find/cljs names))
 
 (defmacro sources-clj
-  "Make a map of namespace name to source, looking for files on the classpath and in jars."
+  "Make a map of CLJ namespace name to source, looking for files on
+  the classpath and in jars."
   [& names]
   (sources* find/clj names))
 
 (defmacro literally
-  "Convert an expression into a string. Useful to allow syntax highlighting of inline CLJS."
+  "Convert an expression into a string. Useful to allow syntax
+  highlighting of inline CLJS."
   [expr]
   (str expr))
